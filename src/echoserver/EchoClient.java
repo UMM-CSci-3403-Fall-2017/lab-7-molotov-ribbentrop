0f5c6b8d000c4f7841b57c6567ed0fa8740cae93
Danish Malik <malik083@morris.umn.edu>
2017-11-05 21:41:53 -0600

    Finish writing 'EchoClient.java'.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -4,25 +4,49 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.*;
 
-public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
+public class EchoClient implements Runnable {
+        public static final int PORT_NUMBER = 6013;
+        private Socket socket;
+        private InputStream socketInputStream;
+        private OutputStream socketOutputStream;
 
-	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
-	}
+        public EchoClient() throws IOException{
+                socket = new Socket("localhost", PORT_NUMBER);
+                socketInputStream = socket.getInputStream();
+                socketOutputStream = socket.getOutputStream();
+        }
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
-		}
-		System.out.flush();
-	}
+        public static void main(String[] args) throws IOException {
+                EchoClient client = new EchoClient();
+                Thread serverListener = new Thread(client, "Client Listener");
+                try{
+                        client.start();
+                        serverListener.start();
+                } catch(SocketException ex){
+                        System.out.println("The connection from the server was lost.");
+                }
+        }
+
+        private void start() throws IOException {
+                int readByte;
+                while ((readByte = System.in.read()) != -1) {
+                        socketOutputStream.write(readByte);
+                }
+                socketOutputStream.flush();
+                socket.shutdownOutput();
+        }
+
+        public void run(){
+                try{
+                        int readByte = -1;
+                        while((readByte = socketInputStream.read()) != -1){
+                                System.out.write(readByte);
+                        }
+                  System.out.flush();
+                } catch(Exception ex){}
+        }
 }
+
+
